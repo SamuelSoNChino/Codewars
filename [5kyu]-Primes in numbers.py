eac0f437707886270dd7c40894ecc6fb2a3f4287
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-04-10 18:05:19 +0200

    Create [5kyu]-Primes in numbers.py

---
+++ [5kyu]-Primes in numbers.py
@@ -0,0 +1,14 @@
+def prime_factors(n):
+    res = ""
+    for i in range(2, n + 1):
+        count = 0
+        while n % i == 0:
+            count += 1
+            n /= i
+        if count == 1:
+            res += "(" + str(i) + ")"
+        elif count:
+            res += "(" + str(i) + "**" + str(count) + ")"
+    return res
+
+print(prime_factors(60), "(2**2)(3**3)(5)(7)(11**2)(17)")
