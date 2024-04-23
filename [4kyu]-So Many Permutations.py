88582a3befff2eb7aca4b043c7f5114a337890cb
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-04-23 20:03:13 +0200

    Update [4kyu]-So Many Permutations.py

--- [4kyu]-So Many Permutations.py
+++ [4kyu]-So Many Permutations.py
@@ -1,29 +1,32 @@
 def permutations(s):
     perms = []
 
-    def generate(perm: str, characters: list):
+    def generate(perm: str, characters: dict):
         if not characters:
             if perm in perms:
                 return
             else:
                 perms.append(perm)
                 return
-        tried = []
-        for char in characters:
-            if char not in tried:
-                tried.append(char)
-                new_characters = characters.copy()
-                new_characters.remove(char)
-                generate(perm + char, new_characters)
+        for char in characters.keys():
+            new_characters = characters.copy()
+            new_characters[char] -= 1
+            if new_characters[char] == 0:
+                new_characters.pop(char)
+            generate(perm + char, new_characters)
         return
-    
-    generate("", [char for char in s])
+
+    chars = {}
+    for char in s:
+        if char not in chars.keys():
+            chars[char] = 1
+        else:
+            chars[char] += 1
+    generate("", chars)
     return perms
 
-print(sorted(permutations('a')), ['a']);
+
+print(sorted(permutations('a')), ['a'])
 print(sorted(permutations('ab')), ['ab', 'ba'])
-print(sorted(permutations('aabb')), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'])
-
-
-            
-        
+print(sorted(permutations('aabb')), [
+      'aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'])
