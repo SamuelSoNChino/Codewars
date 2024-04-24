525844d24270bf99586d78657541e89493487170
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-04-24 21:28:35 +0200

    Update [4kyu]-Sum of Intervals.py

--- [4kyu]-Sum of Intervals.py
+++ [4kyu]-Sum of Intervals.py
@@ -1,26 +1,30 @@
 def sum_of_intervals(intervals: list):
-    summ = 0
-    for interval in intervals:
-        for interval2 in intervals:
-            if interval in intervals and interval2 in intervals:
-                if interval == interval2 and intervals.count(interval) != 1:
-                    intervals.remove(interval2)
-                elif interval[0] < interval2[0] < interval[1]:
-                    intervals.append(
-                        (interval[0], max(interval[1], interval2[1])))
-                    intervals.remove(interval)
-                    intervals.remove(interval2)
-                elif interval[0] < interval2[1] < interval[1]:
-                    intervals.append(
-                        (min(interval[0], interval2[0]), interval[1]))
-                    intervals.remove(interval)
-                    intervals.remove(interval2)
-    for interval in intervals:
-        summ += len(range(interval[0], interval[1]))
-    return summ
+    total_length = 0
+    intervals.sort(key=lambda x: x[0])
+    current_start, current_end = intervals[0][0], intervals[0][1]
+    for start, end in intervals[1:]:
+        if start > current_end:
+            total_length += current_end - current_start + 1
+            current_start, current_end = start, end
+        elif start < current_end:
+            current_end = end
+    total_length =+ current_end - current_start + 1
+    return total_length
 
 
-print(sum_of_intervals([(1, 5), (0, 6)]), 4)
+print(sum_of_intervals([(1, 5), (0, 6)]), 6)
 print(sum_of_intervals([(1, 5)]), 4)
 print(sum_of_intervals([(1, 5), (1, 5)]), 4)
 print(sum_of_intervals([(1, 4), (7, 10), (3, 5)]), 7)
+print(sum_of_intervals( [
+   [1, 5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+] ))
+print(sum_of_intervals(( [
+   [0, 20],
+   [-100000000, 10],
+   [30, 40]
+] )))
